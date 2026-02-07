23044ce54d0af370899fa0f8c190a1073e80d0d0
Thibaut16 <47196047+Thibaut16@users.noreply.github.com>
2026-02-07 22:35:19 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -8,31 +8,6 @@ Script purpose:
 */ 
 use DatabaseWarehouse;
 
-SELECT TOP (1000) [cst_id]
-      ,[cst_key]
-      ,[cst_firstname]
-      ,[cst_lastname]
-      ,[cst_material_status]
-      ,[cst_gndr]
-      ,[cst_dreate_date]
-  FROM [DatabaseWarehouse].[bronze].[crm_cust_info]
-
-
-SELECT TOP (1000) [prd_id]
-      ,[prd_key]
-      ,[prd_nm]
-      ,[prd_cost]
-      ,[prd_line]
-      ,[prd_start_dt]
-      ,[prd_end_dt]
-  FROM [DatabaseWarehouse].[bronze].[crm_prd_info]
-
-SELECT TOP (1000) * FROM [DatabaseWarehouse].[bronze].[crm_sales_details]
-
-SELECT TOP (1000) * FROM [DatabaseWarehouse].[bronze].[erp_cust_az12]
-SELECT TOP (1000) * FROM [DatabaseWarehouse].[bronze].[erp_loc_a101]
-SELECT TOP (1000) * FROM [DatabaseWarehouse].[bronze].[erp_px_cat_g1v2]
-
 
 --Create table in silver layer
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
